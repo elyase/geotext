20ef8fbd32dac15d4969c85c318ba770fda18e1a
Joel Benjamin <joelbenjamin093@gmail.com>
2016-12-24 23:58:02 +0530

    Importing error with python3
    When importing GeoText, python3 gives an ImportError. To fix that changed it into from geotext import *
    It works when everything is imported.

--- geotext/__init__.py
+++ geotext/__init__.py
@@ -4,4 +4,11 @@ __author__ = 'Yaser Martinez Palenzuela'
 __email__ = 'yaser.martinez@gmail.com'
 __version__ = '0.1.0'
 
-from geotext import GeoText
+#from geotext import GeoText
+# That gives an ImportError when importing
+# Don't know why that happens.
+
+#instead import everything, works well.
+from geotext import *
+
+
