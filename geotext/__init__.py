8c47a8882a97b0cc0d6ae5fa512c2960e8ce030b
Yaser Martinez Palenzuela <yaser@needls.com>
2017-01-07 00:12:08 +0100

    Fix relative import

--- geotext/__init__.py
+++ geotext/__init__.py
@@ -4,11 +4,4 @@ __author__ = 'Yaser Martinez Palenzuela'
 __email__ = 'yaser.martinez@gmail.com'
 __version__ = '0.1.0'
 
-#from geotext import GeoText
-# That gives an ImportError when importing
-# Don't know why that happens.
-
-#instead import everything, works well.
-from geotext import *
-
-
+from .geotext import GeoText
