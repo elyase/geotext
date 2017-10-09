58c2cf399875ff0b6fb576321e0688cddbd1195a
Devin Prescott <devincprescott@gmail.com>
2017-10-09 10:29:05 -0400

    bug fix: encoding missing from open()
    Encoding option that is passed to read_table was never passed to open() command.
    
    Should fix ISSUE #8: UnicodeDecodeError: 'gbk' codec can't decode byte 0xbf in position 2: illegal multibyte sequence

--- geotext/geotext.py
+++ geotext/geotext.py
@@ -42,7 +42,7 @@ def read_table(filename, usecols=(0, 1), sep='\t', comment='#', encoding='utf-8'
     A dictionary with the same length as the number of lines in `filename`
     """
 
-    with open(filename, 'r') as f:
+    with open(filename, 'rt', encoding=encoding) as f:
         # skip initial lines
         for _ in range(skip):
             next(f)
